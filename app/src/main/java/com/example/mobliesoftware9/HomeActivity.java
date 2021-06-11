42bd2cee5c9c906f61ba4e2c176c1a870da05522
sundui321 <sundui_321@yahoo.com>
2021-06-12 00:14:37 +0900

    HomeActivity 추가, activity_home.xml  구현 중

---
+++ app/src/main/java/com/example/mobliesoftware9/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.mobliesoftware9;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // 오늘의 사진이나 설정, 글조희 등 기능들로 이동
+
+    }
+
+}
