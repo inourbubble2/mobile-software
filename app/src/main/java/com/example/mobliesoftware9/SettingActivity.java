7af61f80d7bfc7187b5b8546a5622088732e4e54
Nayeon Kim <inourbubble2@gmail.com>
2021-06-13 01:51:46 +0900

    feat: changing username

--- app/src/main/java/com/example/mobliesoftware9/SettingActivity.java
+++ app/src/main/java/com/example/mobliesoftware9/SettingActivity.java
@@ -3,24 +3,48 @@ package com.example.mobliesoftware9;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.mobliesoftware9.model.User;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 //
 public class SettingActivity extends AppCompatActivity {
 
+    EditText username;
+    EditText password;
+    Button btnSaveProfile;
+    User currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
         // 세팅과 관련된 액티비티
-        
         // 알림 설정, 닉네임 변경 등 진행
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        btnSaveProfile = (Button) findViewById(R.id.btnSaveProfile);
+        currentUser = User.getInstance();
+
+        username.setText(currentUser.username);
+
+
+        btnSaveProfile.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String usernameStr = username.getText().toString();
+                currentUser.username = usernameStr;
+            }
+        });
+
+
 
         //navbar variable
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navbar);
