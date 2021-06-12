4d3bbe4b3c60f8ade3e56f40a617df73be120321
sundui321 <sundui_321@yahoo.com>
2021-06-12 20:12:35 +0900

    navbar과 화면 이동

--- app/src/main/java/com/example/mobliesoftware9/BoardActivity.java
+++ app/src/main/java/com/example/mobliesoftware9/BoardActivity.java
@@ -1,17 +1,52 @@
 package com.example.mobliesoftware9;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BoardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_board);
 
         // 내가 쓴 글 목록 조회
+
+        //navbar variable
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navbar);
+
+        //default selected activity
+        bottomNav.setSelectedItemId(R.id.nav_board);
+
+        //activity간의 이동
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch(menuItem.getItemId()) {
+                    case R.id.nav_home:
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_readPost:
+                        startActivity(new Intent(getApplicationContext(), ReadPostActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_board:
+                        return true;
+                    case R.id.nav_settings:
+                        startActivity(new Intent(getApplicationContext(), SettingActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                }
+                return false;
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/mobliesoftware9/ReadPostActivity.java
+++ app/src/main/java/com/example/mobliesoftware9/ReadPostActivity.java
@@ -1,18 +1,53 @@
 package com.example.mobliesoftware9;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ReadPostActivity  extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_read);
 
         // 글 조회
         // 댓글 관련 부분도 함께 있어야 됨
+
+        //navbar variable
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navbar);
+
+        //default selected activity
+        bottomNav.setSelectedItemId(R.id.nav_readPost);
+
+        //activity간의 이동
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch(menuItem.getItemId()) {
+                    case R.id.nav_home:
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_readPost:
+                        return true;
+                    case R.id.nav_board:
+                        startActivity(new Intent(getApplicationContext(), BoardActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_settings:
+                        startActivity(new Intent(getApplicationContext(), SettingActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                }
+                return false;
+            }
+        });
     }
 
 }
