ef19152b9331068de6696d14deb121ee65d8ce42
Nayeon Kim <inourbubble2@gmail.com>
2021-06-17 23:00:50 +0900

    feat: Slide에 이미지 나오게 하기

--- app/src/main/java/com/example/mobliesoftware9/SlideAdapter.java
+++ app/src/main/java/com/example/mobliesoftware9/SlideAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +17,10 @@ import android.widget.Toast;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewpager.widget.PagerAdapter;
 
+import com.example.mobliesoftware9.DB.CursorWrapper;
+import com.example.mobliesoftware9.DB.DatabaseManager;
 import com.example.mobliesoftware9.model.Post;
+import com.example.mobliesoftware9.model.User;
 
 //readpost 내에서 유저들의 post slider
 public class SlideAdapter extends PagerAdapter {
@@ -41,17 +45,19 @@ public class SlideAdapter extends PagerAdapter {
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
+        Post post = dataSet[position];
+
         inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.slide, container, false);
-        RelativeLayout layoutSlide = (RelativeLayout) view.findViewById(R.id.constraintLayoutSlide);
+        ConstraintLayout layoutSlide = (ConstraintLayout) view.findViewById(R.id.constraintLayoutSlide);
         ImageView imgSlide = (ImageView) view.findViewById(R.id.slideImg);
+        ImageView imgPostUser = (ImageView) view.findViewById(R.id.imgPostUser);
         TextView postSlide = (TextView) view.findViewById(R.id.userPostTitle);
         TextView postContent = (TextView) view.findViewById(R.id.userPost);
         TextView postUser = (TextView) view.findViewById(R.id.postUsername);
         TextView likedCount = (TextView) view.findViewById(R.id.noOfLike);
 
 
-        Post post = dataSet[position];
         imgSlide.setImageBitmap(post.attachedImg.mBitmap);
         postSlide.setText(post.title);
         postContent.setText(post.content);
@@ -65,6 +71,22 @@ public class SlideAdapter extends PagerAdapter {
 
         Button btnComment = (Button) view.findViewById(R.id.btnComment);
         Button btnShare = (Button) view.findViewById(R.id.btnShare);
+
+
+        // 작성자 프로필 사진 뜨게 하기
+        CursorWrapper userCursor = DatabaseManager.GetInstance().SelectRows("User",
+                null,
+                new String[]{"username"},
+                new String[]{post.writerID},
+                null,
+                null);
+        userCursor.mCursor.moveToNext();
+        User poster = new User();
+        poster.LoadFromCursor(userCursor);
+
+        if (poster.mProfileImage.mBitmap != null) {
+            imgPostUser.setImageBitmap(poster.mProfileImage.mBitmap);
+        }
 
         btnLike.setOnClickListener(v -> {
             if (post.OnClickLikeButton()) {
